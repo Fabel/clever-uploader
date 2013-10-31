3cf6d8a6fe10c66ca7c3b6ec08653b9ab201e34e
FoboCasteR <fobocaster@gmail.com>
2013-10-31 23:59:54 +0300

    disable precompile

--- lib/clever_uploader/rails.rb
+++ lib/clever_uploader/rails.rb
@@ -1,7 +1,4 @@
 module CleverUploader
   class Engine < Rails::Engine
-    initializer :assets, :group => :all do |app|
-      app.config.assets.precompile += %w( clever-uploader.js )
-    end
   end
 end
